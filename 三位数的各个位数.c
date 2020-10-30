b57e401a2a54f63d85f9e0c1e0ec103a11ca3276
JiangYuxin-18 <72787959+JiangYuxin-18@users.noreply.github.com>
2020-10-30 14:05:33 +0800

    Create 三位数的各个位数.c

---
+++ 三位数的各个位数.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int  x;
+	int  a,b,c;
+	printf("请任意输入一个三位数：\n");
+	scanf("%d",&x);
+	a = x/100;
+	b = x/10%10;
+	c = x%10;
+
+    printf("%d\n%d\n%d\n",c,b,a);
+    return 0;
+}
