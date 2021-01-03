269f468bdfeef2cff16423d2ca1eb9c3dc96d9fe
JiangYuxin-18 <72787959+JiangYuxin-18@users.noreply.github.com>
2021-01-03 23:48:27 +0800

    Add files via upload

---
+++ 最小的数.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+	int i=0,a[11]={0};
+	
+	for(i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<10;i++){
+		if(a[i]!=0){
+			printf("%d",i);
+			a[i]--;
+			break;
+		}
+	}
+	for(i=0;i<10;i++){
+		while(a[i]!=0){
+			printf("%d",i);
+			a[i]--;
+		}
+	}
+	printf("\n");
+	
+}
